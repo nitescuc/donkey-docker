f958c00db3c35d2c1ae01e217048f29571547bd9
NITESCU Cristian <cristian.nitescu@renault.com>
2018-10-26 22:18:45 +0200

    Added port definition

--- drive.sh
+++ drive.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -v ~/d2:/d2 -v ~/donkey:/donkey --device=/dev/video0:/dev/video0 donkey:3.0.0_1.8.0 python /d2/manage.py drive $1
+docker run -it --rm -p 8887:8887 -v ~/d2:/d2 -v ~/donkey:/donkey --device=/dev/video0:/dev/video0 donkey:3.0.0_1.8.0 python /d2/manage.py drive $1
