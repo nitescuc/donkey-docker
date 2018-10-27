eb43b9c48cf37d98104a07e6b7d1366efcb2bc70
NITESCU Cristian <cristian.nitescu@renault.com>
2018-10-28 00:18:17 +0200

    Device and port settings in drive.sh

--- drive.sh
+++ drive.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -p 8887:8887 -v ~/d2:/d2 -v ~/donkey:/donkey --device=/dev/video0:/dev/video0 donkey:3.0.0_1.8.0 python /d2/manage.py drive $1
+docker run -it --rm -p 8887:8887 -v ~/d2:/d2 -v ~/donkey:/donkey --device=/dev/video0:/dev/video0 --device=/dev/ttyACM0:/dev/ttyACM0 tazlogic/donkey:3.0.0_1.8.0 python /d2/manage.py drive $1
